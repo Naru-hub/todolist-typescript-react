94a306b8e086f0c63a303d594627776e38e00a6d
Naru-hub <1208ny0324@gmail.com>
2022-08-22 20:40:02 +0900

    見ための部分を作成

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,17 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div>
+        <h2>TodoList with TypeScript</h2>
+        <form onSubmit={() => {}}>
+          <input type="text" onChange={() => {}} className="inputText" />
+          <input type="submit" value="作成" className="submitButton" />
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
